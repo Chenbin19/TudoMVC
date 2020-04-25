842e4f1c64725b5b6cd70e2b8c328f96b20b4c19
chenbin <952177159@qq.com>
2020-04-25 15:38:57 +0800

    todoMVC

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,15 +1,18 @@
-(function (Vue) {  // 表示依赖了全局的vue
+(function (Vue) {
 
-	// Your starting point. Enjoy the ride!
+	//本地存储，获取
+	let itemStorage = {
+		// 获取数据
+		fetch() {
+			return JSON.parse(localStorage.getItem("item") || '[]')
 
-	const items = [
-		{ id: 1, content: "湖人MVP", completed: false },
-		{ id: 2, content: "詹姆斯", completed: false },
-		{ id: 3, content: "戴维斯", completed: false }
-	]
+		},
+		// 保存数据
+		save(items) {
+			localStorage.setItem("item", JSON.stringify(items))
+		}
+	}
 
-<<<<<<< HEAD
-=======
 	//注册全局指令
 	Vue.directive('app-focus', {
 		inserted(el, binding) {
@@ -17,17 +20,13 @@
 			el.focus()
 		}
 	})
->>>>>>> clear completed
 
-	new Vue({
+	let app = new Vue({
 		el: '#todoapp',
 		data: {
-<<<<<<< HEAD
-			items
-		},
-=======
-			items,
-			currentItem: null
+			items: itemStorage.fetch(),//获取本地数据进行初始化
+			currentItem: null, // 点击的那个任务项
+			filterStatus: 'all' // 接收变化的状态值
 		},
 
 		// 自定义局部指令
@@ -41,18 +40,42 @@
 				}
 			}
 		},
-
->>>>>>> clear completed
+		// 监听器
+		watch: {
+			// 如果 items 发生改变，这个函数就会运行
+			items: {
+				deep: true, // 发现对象内部值的变化, 要在选项参数中指定 deep: true。
+				handler: function (newItems, oldItems) {
+					//本地进行存储
+					itemStorage.save(newItems)
+				}
+			}
+		},
 		// 定义计算属性
 		computed: {
+			// 根据不同状态过滤出不同数据
+			filterItems() {
+				// 判断filterItems状态值
+				switch (this.filterStatus) {
+					case 'active':
+						// 过滤出未完成的数据
+						return this.items.filter(item => !item.completed)
+						break;
+					case 'completed':
+						// 过滤出已完成的数据
+						return this.items.filter(item => item.completed)
+						break;
+
+					default:
+						return this.items
+						break;
+				}
+			},
+
 			toggleAll: {
 				// 当任务列表中的状态发生变化后，就更新复选框状态
 				get() {
-<<<<<<< HEAD
-					console.log('get', this.remaining)
-=======
 					// console.log('get', this.remaining)
->>>>>>> clear completed
 					return this.remaining === 0
 				},
 				// 当复选框状态更新后，任务列表中的状态更新
@@ -70,7 +93,7 @@
 			remaining() {
 				// 数组filter函数过滤所有未完成的任务项
 				// unItems 用于接收过滤之后未完成的任务项，它是一个数组
-				let unItems = this.items.filter(function (item) {
+				let unItems = this.items.filter(item => {
 					return !item.completed
 				})
 				return unItems.length
@@ -79,15 +102,13 @@
 		// 定义方法
 		methods: {
 
-<<<<<<< HEAD
-=======
 			// 完成编辑，保存数据
 			finishEdit(item, index, event) {
 				// 1.获取当前输入框的值
 				let content = event.target.value.trim()
 				// 2.判断输入框的值是否为空，如果为空就删除任务项
 				if (!content) {
-					// 服用下面删除函数
+					// 复用下面删除函数
 					this.removeItem(index)
 					return
 				}
@@ -107,7 +128,6 @@
 				this.currentItem = item
 			},
 
->>>>>>> clear completed
 			// 删除任务项
 			removeItem(index) {
 				this.items.splice(index, 1)
@@ -120,7 +140,7 @@
 
 			// 添加文本框
 			addItem(evevt) {
-				// console.log("addItem", event.target.value)
+				console.log("addItem", event.target.value)
 				// 1.获取文本框中的内容
 				let content = event.target.value.trim() //将空格去掉
 				// 2.判断数据是否为空，如果为空，什么都不做
@@ -128,7 +148,7 @@
 					return
 				}
 				// 3.如果不为空，添加到数组中
-				const id = this.items.lengt + 1
+				let id = this.items.length + 1
 				this.items.push(
 					{ id, content, completed: false }
 				)
@@ -138,5 +158,16 @@
 
 		}
 	})
+	// 当路由状态变化调用
+	window.onhashchange = function () {
+		// console.log("hash", window.location.hash)
+		// 获取路由hash，截取hash值，不为空返回，为空返回all
+		let hash = window.location.hash.substr(2) || 'all'
+		// 状态发生改变，就将hash赋值给filterStatus
+		// 定义计算属性filterItems来感知filterStatus的变化，当它变化时过滤出不同数组
+		app.filterStatus = hash
+	}
+	// 页面一刷新就调用，让状态值生效
+	window.onhashchange()
 
 })(Vue);
